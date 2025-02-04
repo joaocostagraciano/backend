88188c3191fb985361287812f7bfad81f245a2ac
joaocostagraciano <joao.costa.graciano@gmail.com>
2025-02-04 14:59:28 -0300

    aula 1-backend

---
+++ alunos/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Estado(models.Model):
+    nome = models.CharField(max_length=50)
+    sigla = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.nome} ({self.sigla})"
